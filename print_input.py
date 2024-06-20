7075b768a51467b85e3b471ba9291b1753796c47
Giovanna Antonieli <129458706+GiihAntonieli@users.noreply.github.com>
2024-06-20 18:59:13 -0300

    v1.0

---
+++ print_input.py
@@ -0,0 +1,10 @@
+nome = input('Informe o seu nome: ')
+idade  = input('Informe a sua idade: ')
+
+print(nome, idade)
+print(nome, idade, end="... \n")#No final o end adiciona algo no final 
+print(nome, idade, sep="#")#sep = Separador no meio das variáveis acrescenta algo
+print(nome, idade, sep="#", end="... \n")
+print(nome, idade, end="\n")
+print('Alguma coisa')
+print('Tentando fazer algo')
---
+++ variaveis_e_constantes.py
@@ -0,0 +1,14 @@
+nome = 'Giovanna'
+idade = 18
+
+nome, idade = ('Paulo', 27)#Parenteses são opcionais
+
+print(nome, idade)
+
+limite_saque_diario = 1000 #Use esse padrão no PYTHON (Snake Case)
+limiteSaqueDiario = 1000 #Esse padrão é utilizado em outras linguagens como Java, JavaScript ... (Camel Case)
+#Coloque nomes sugestivos (Não tenha medo de nomes grandes, o importante é que eles sejam de fácil entendimento)
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS'] #Constantes em PYTHON são representadas com nomes em letras maiúsculas
+
+print(BRAZILIAN_STATES)
